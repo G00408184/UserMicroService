5aa6c9d31c68a3eb6f3c5b354882adeea5585598
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2024-10-29 15:47:38 +0000

    CreatingEntity

--- src/main/java/com/example/usermicroservice/Entity/User.java
+++ src/main/java/com/example/usermicroservice/Entity/User.java
@@ -2,6 +2,7 @@ package com.example.usermicroservice.Entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "user")
@@ -17,15 +18,21 @@ public class User {
     @Column(name = "idUser")
     private Long id;
     @Column(name = "name")
+    @NotBlank(message = "Please add your name here")
     private String  name;
+    @NotBlank(message = "Please add your date of birth here")
     @Column(name = "DateOfBirth")
     private String  DateOfBirth;
+    @NotBlank(message = "Please add your password here")
     @Column(name = "Password")
     private String  Password;
+    @NotBlank(message = "Please add your email here")
     @Column(name = "Email")
     private String  Email;
+    @NotBlank(message = "Please add your address here")
     @Column(name = "Address")
     private String  Address;
+    @NotBlank(message = "Please add your phone number here")
     @Column(name = "phonenumber")
     private int  PhoneNumber;
     @Column(name = "isAdmin")
