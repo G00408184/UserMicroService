7122168c73a23b2835657290c9ed783471b9ad8e
Jakub Mamala <g00408184@atu.ie>
2025-01-06 20:12:22 +0000

    Setup test clas for userService

---
+++ src/test/java/com/example/usermicroservice/Service/UserServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.usermicroservice.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+}
